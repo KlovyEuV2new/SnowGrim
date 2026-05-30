db60b5cb3f7211ebeed9779184bf63aa1c5e3dcf
KlovyEuV2new <reona4458@gmail.com>
2026-05-30 18:04:10 +0500

    FlightB.java check fix

--- common/src/main/java/ac/grim/grimac/checks/impl/flight/FlightB.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/flight/FlightB.java
@@ -5,13 +5,10 @@ import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
-import ac.grim.grimac.utils.data.attribute.ValuedAttribute;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
-import com.github.retrooper.packetevents.protocol.attribute.Attributes;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.GameMode;
 
-import java.util.Optional;
 
 @CheckData(name = "FlightB", setback = 0, description = "Flight speed check")
 public class FlightB extends Check implements PacketCheck {
@@ -56,12 +53,10 @@ public class FlightB extends Check implements PacketCheck {
     }
 
     private double getMaxSpeed() {
-        Optional<ValuedAttribute> flyingAttr = player.compensatedEntities.self.getAttribute(Attributes.GENERIC_FLYING_SPEED);
-        double attributeSpeed = (flyingAttr != null && flyingAttr.isPresent()) ? flyingAttr.get().get() : 0.0;
+        //Optional<ValuedAttribute> flyingAttr = player.compensatedEntities.self.getAttribute(Attributes.GENERIC_FLYING_SPEED);
         double flySpeed = player.bukkiFlySpeed != Float.MAX_VALUE ? player.bukkiFlySpeed : player.flySpeed;
-        if (player.flySpeed == Integer.MAX_VALUE) flySpeed = 1.0;
         double base = 1.2;
-        if ((flyingAttr != null && flyingAttr.isPresent()) || player.gamemode.equals(GameMode.SPECTATOR)) {
+        if (player.gamemode.equals(GameMode.SPECTATOR)) {
             return Integer.MAX_VALUE;
         }
         return base * flySpeed * 10.0;
